87926207e5596ffe20fb673bba89205cc1807d08
Pedro Enciso <penciso@ucsd.edu>
2021-11-24 19:09:37 -0800

    full app

---
+++ server/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// handle production
+if (process.env.NODE_ENV === "production") {
+  // static folder
+  app.use(express.static(__dirname + "./public/"));
+
+  // handle SPA
+  app.get("./*/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+}
+
+// routes
+const posts = require("./routes/api/posts");
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
